0fb017968d703d15ed7de235bbdcc1b221558fec
Admin <admin@Admins-MacBook-Air.local>
2019-02-20 19:30:48 -0800

    Added CORS middleware

--- app.js
+++ app.js
@@ -1,51 +1,55 @@
-const express = require('express')
-const Todo = require('./db/todo.model')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const express = require("express");
+const Todo = require("./db/todo.model");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+var cors = require("cors");
 
-const app = express()
+// Intialize express app
+const app = express();
 
-app.use(bodyParser.json())
-app.unsubscribe(bodyParser.urlencoded({ extended: false }))
+// Add CORS & bodyParser middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.unsubscribe(bodyParser.urlencoded({ extended: false }));
 
 mongoose.connect(
-  'mongodb://lucas:42marvin@ds133865.mlab.com:33865/heroku_m1jp2bks',
+  "mongodb://lucas:42marvin@ds133865.mlab.com:33865/heroku_m1jp2bks",
   { useNewUrlParser: true }
-)
-const connection = mongoose.connection
-connection.once('open', function () {
-  console.log('MongoDB database connection established successfully')
-})
+);
+const connection = mongoose.connection;
+connection.once("open", function() {
+  console.log("MongoDB database connection established successfully");
+});
 
-app.get('/api/v1/todos', (req, res) => {
-  Todo.find(function (err, todos) {
+app.get("/api/v1/todos", (req, res) => {
+  Todo.find(function(err, todos) {
     if (err) {
-      console.log(err)
+      console.log(err);
     } else {
-      console.log(todos)
+      console.log(todos);
 
-      res.json(todos)
+      res.json(todos);
     }
-  })
-})
+  });
+});
 
 // Get single todo by id
-app.get('/api/v1/todos/:id', (req, res) => {
-  let id = req.params.id
-  Todo.findById(id, function (err, todo) {
-    res.json(todo)
-  })
-})
+app.get("/api/v1/todos/:id", (req, res) => {
+  let id = req.params.id;
+  Todo.findById(id, function(err, todo) {
+    res.json(todo);
+  });
+});
 
 //post request which adds a todo
 
-app.post('/api/v1/todos', (req, res) => {
-  Todo.create(req.body, function (err) {
+app.post("/api/v1/todos", (req, res) => {
+  Todo.create(req.body, function(err) {
     if (err) {
-      res.status(400).send('adding new todo failed')
+      res.status(400).send("adding new todo failed");
     }
-  })
-  res.status(200).json({ todo: 'todo added successfully' })
+  });
+  res.status(200).json({ todo: "todo added successfully" });
 
   //   console.log(req.body)
   //  let todo = new Todo(req.body);
@@ -57,45 +61,45 @@ app.post('/api/v1/todos', (req, res) => {
   //       .catch(err => {
   //           res.status(400).send('adding new todo failed');
   //       });
-})
+});
 
-app.post('/api/v1/todos/:id', (req, res) => {
-  Todo.findById(req.params.id, function (err, todo) {
-    if (!todo) res.status(404).send('data is not found')
+app.post("/api/v1/todos/:id", (req, res) => {
+  Todo.findById(req.params.id, function(err, todo) {
+    if (!todo) res.status(404).send("data is not found");
     // Update for new model
-    else todo.description = req.body.description
-    todo.title = req.body.title
-    todo.completed = req.body.completed
+    else todo.description = req.body.description;
+    todo.title = req.body.title;
+    todo.completed = req.body.completed;
 
     todo
       .save()
       .then(todo => {
-        res.json('Todo updated')
+        res.json("Todo updated");
       })
       .catch(err => {
-        res.status(400).send('Update not possible')
-      })
-  })
-})
+        res.status(400).send("Update not possible");
+      });
+  });
+});
 
 //deletes a todo
-app.delete('/api/v1/todos/:id', (req, res) => {
-  const id = req.params.id
+app.delete("/api/v1/todos/:id", (req, res) => {
+  const id = req.params.id;
 
   // Remove by id
-  Todo.deleteOne({ _id: id }, function (err) {
+  Todo.deleteOne({ _id: id }, function(err) {
     if (err) {
       return res.status(404).send({
-        success: 'false',
-        message: 'todo not found',
-      })
+        success: "false",
+        message: "todo not found"
+      });
     }
     return res.status(200).send({
-      success: 'true',
-      message: 'todo was removed',
-    })
-  })
-})
+      success: "true",
+      message: "todo was removed"
+    });
+  });
+});
 
 // //updating the todo.
 // app.put('/api/v1/todos/:id', (req, res) => {
@@ -131,8 +135,8 @@ app.delete('/api/v1/todos/:id', (req, res) => {
 //   });
 // });
 
-const PORT = 5000
+const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
-})
+  console.log(`server running on port ${PORT}`);
+});
